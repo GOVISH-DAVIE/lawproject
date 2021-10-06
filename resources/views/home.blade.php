8b32e56a2dd92fb7f1584158f1c7add09c36fc0d
Govish <9davidmuia@gmail.com>
2021-10-06 13:53:02 +0300

    records forms

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,64 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card shadow">
+                    <div class="card-header">{{ __('Add A document') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
+                    <div class="card-body">
+
                         <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                            <form action="recods" method="POST" enctype="multipart/form-data">
+                                {{ @csrf }}
+                                <div class="mb-3">
+                                    <label for="exampleFormControlInput1" class="form-label"> Title </label>
+                                    <input type="text" class="form-control" id="exampleFormControlInput1"
+                                        placeholder="Title">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="exampleFormControlInput1" class="form-label"> Date</label>
+                                    <input type="date" class="form-control" id="exampleFormControlInput1">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="exampleFormControlInput1" class="form-label"> Location</label>
+                                    <input type="text" class="form-control" id="exampleFormControlInput1"
+                                        placeholder="KIlimani..">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="exampleFormControlInput1" class="form-label"> Charges(KES) </label>
+                                    <input type="number" class="form-control" id="exampleFormControlInput1">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="exampleFormControlTextarea1" class="form-label">Decription
+                                        textarea</label>
+                                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                                </div>
+                                <div class="mb-3">
+                                    <label for="exampleFormControlInput1" class="form-label">Client Email
+                                        Address</label>
+                                    <input type="email" class="form-control" id="exampleFormControlInput1"
+                                        placeholder="name@example.com">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="exampleFormControlInput1" class="form-label">Client Telephone
+                                        Number</label>
+                                    <input type="email" class="form-control" id="exampleFormControlInput1"
+                                        placeholder="name@example.com">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="exampleFormControlInput1" class="form-label">Client Telephone
+                                        Number</label>
+                                    <input type="file" class="form-control" id="exampleFormControlInput1"
+                                        placeholder="name@example.com">
+                                </div>
+                                <button class="btn btn btn-outline-secondary btn-lg"> Create Entry</button>
+                            </form>
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
