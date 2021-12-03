fd7edaa9bebd8cf23cfe56eb1359a9b6286ea0d6
Govish <9davidmuia@gmail.com>
2021-12-03 20:18:09 +0300

    alot

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -64,10 +64,13 @@
 
                                 <div class="form-group row mb-0">
                                     <div class="col-md-8 offset-md-4">
-                                        <button type="submit" class="btn btn-primary">
+                                        <button type="submit" class="btn btn-lg btn-outline-dark">
                                             {{ __('Login') }}
                                         </button>
-
+                                        <br>
+                                        <a class="btn btn-link" href="{{ route('register') }}">
+                                            {{ __('Create Account?') }}
+                                        </a>
                                         @if (Route::has('password.request'))
                                             <a class="btn btn-link" href="{{ route('password.request') }}">
                                                 {{ __('Forgot Your Password?') }}
